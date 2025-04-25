155372553c228d70de6e85a284fa1dfee24ebb18
Deepthi <deepthi.sridevi@navayuvabharatinfotech.com>
2025-04-25 19:08:21 +0530

    constraints

---
+++ Daywise_practice/webDevelopment/string.js
@@ -0,0 +1,21 @@
+var a="Hello world";
+console.log(a.replace("world","javascript"));
+
+var a="hello";
+var b="world";
+console.log(a+' '+b);
+console.log(b+' '+a);
+console.log(a.concat(b));
+ var a="apple,banana,mango,pineapple";
+ var s= a.split(",");
+ console.log(s);
+ var a="apple banana mango pineapple";
+
+ var s= a.split(",");
+ console.log(s);
+ var b="banana"
+ console.log(a.indexOf(b));
+console.log(a.startsWith("banana"));
+console.log(a.endsWith("pineapple"));
+
+
