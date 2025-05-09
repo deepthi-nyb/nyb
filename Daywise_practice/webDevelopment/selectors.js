0141230dbd0bb84c2131960f0a28f42331a814ab
Deepthi <deepthi.sridevi@navayuvabharatinfotech.com>
2025-05-09 18:07:53 +0530

    index_selector

---
+++ Daywise_practice/webDevelopment/selectors.js
@@ -0,0 +1,7 @@
+var a=document.getElementById("main title");
+console.dir(a.textContent);
+// var boxes=document.getElementsByClassName("box");
+// console.log(boxes[0].textContent);
+
+let boxes = document.getElementsByClassName("box");
+console.log(boxes[0].textContent);
